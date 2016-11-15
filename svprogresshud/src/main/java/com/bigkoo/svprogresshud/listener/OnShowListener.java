22c8da8252167b308f92ebe6bc42f86185f56144
longe <a0691401@126.com>
2016-11-15 10:39:53 +0800

    v1.0

---
+++ svprogresshud/src/main/java/com/bigkoo/svprogresshud/listener/OnShowListener.java
@@ -0,0 +1,10 @@
+package com.bigkoo.svprogresshud.listener;
+
+import com.bigkoo.svprogresshud.SVProgressHUD;
+
+/**
+ * Created by apple on 16/11/15.
+ */
+public interface OnShowListener {
+    void onShow(SVProgressHUD hud);
+}
